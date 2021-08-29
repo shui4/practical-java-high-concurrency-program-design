278568138fa800c6fc35672c45dc591222535c30
shui4 <1078185271@qq.com>
2021-08-29 22:32:23 +0800

    feature: 不变模式

--- src/main/java/indi/shui4/ch5/parallel_mode_and_algorithm/immutable/Product1.java
+++ src/main/java/indi/shui4/ch5/parallel_mode_and_algorithm/immutable/Product1.java
@@ -4,26 +4,25 @@ package indi.shui4.ch5.parallel_mode_and_algorithm.immutable;
  * @author shui4
  * @since 1.0
  */
-public final class Product {
+public final class Product1 {
   private final String name;
   private final String no;
   private final double price;
 
-  public Product(String no, String name, double price) {
+  public Product1(String no, String name, double price) {
     this.no = no;
     this.name = name;
     this.price = price;
   }
-  
-  
+
   public String getName() {
     return name;
   }
-  
+
   public String getNo() {
     return no;
   }
-  
+
   public double getPrice() {
     return price;
   }
