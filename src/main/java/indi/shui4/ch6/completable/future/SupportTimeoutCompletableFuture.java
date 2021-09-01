1516ee74fbbaa648ff404611723140c400a7994d
shui4 <1078185271@qq.com>
2021-09-01 19:35:54 +0800

    feature: 6.5.6 支持timeout的 CompletableFuture

---
+++ src/main/java/indi/shui4/ch6/completable/future/SupportTimeoutCompletableFuture.java
@@ -0,0 +1,31 @@
+package indi.shui4.ch6.completable.future;
+
+import lombok.SneakyThrows;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author shui4
+ * @since 1.0
+ */
+public class SupportTimeoutCompletableFuture {
+
+  @SneakyThrows
+  public static void main(String[] args) {
+    CompletableFuture<Integer> future = CompletableFuture
+        .supplyAsync(
+            () -> {
+              try {
+                TimeUnit.SECONDS.sleep(2);
+              } catch (InterruptedException e) {
+              }
+              return calc(50);
+            });
+    future.get(1,TimeUnit.SECONDS);
+  }
+
+  public static Integer calc(Integer para) {
+    return para / 2;
+  }
+}
